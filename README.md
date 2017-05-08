8aa134f7f7cad6a9162f4acc252cfcd37a7fe8b1
Jie.Liu <ecliujie@gmail.com>
2017-05-08 14:39:29 +0800

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# learn-resource
+
+> learn-resource
+
+## getting start
+
+```bash
+npm install
+```
+
+
+## npm scripts
+
+```bash
+# build both two js bundles for Weex and Web
+npm run build
+
+# build the two js bundles and watch file changes
+npm run dev
+
+# start a Web server at 8085 port
+npm run serve
+
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var bannerPlugin = new webpack.BannerPlugin(
+  '// { "framework": "Vue" }\n',
+  { raw: true }
+)
+
+function getBaseConfig () {
+  return {
+    entry: {
+      app: path.resolve('./app.js')
+    },
+    output: {
+      path: 'dist',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel',
+          exclude: /node_modules/
+        }, {
+          test: /\.vue(\?[^?]+)?$/,
+          loaders: []
+        }
+      ]
+    },
+    vue: {
+      // // You can use PostCSS now!
+      // // Take cssnext for example:
+      // // 1. npm install postcss-cssnext --save-dev
+      // // 2. write `var cssnext = require('postcss-cssnext')` at the top
+      // // 3. set the config below
+      // postcss: [cssnext({
+      //   features: {
+      //     autoprefixer: false
+      //   }
+      // })]
+    },
+    plugins: [bannerPlugin]
+  }
+}
+
+var webConfig = getBaseConfig()
+webConfig.output.filename = '[name].web.js'
+webConfig.module.loaders[1].loaders.push('vue')
+
+var weexConfig = getBaseConfig()
+weexConfig.output.filename = '[name].weex.js'
+weexConfig.module.loaders[1].loaders.push('weex')
+
+module.exports = [webConfig, weexConfig]
